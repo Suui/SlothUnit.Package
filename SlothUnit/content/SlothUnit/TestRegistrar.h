550f39f542a9aa5f1f05144cacafacd8cccdf77b
Suui <suui.gd@gmail.com>
2017-01-23 02:20:44 +0100

    Starting point from the previous research spike

---
+++ SlothUnit/content/SlothUnit/TestRegistrar.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "SlothTests.h"
+#include "TestClass.h"
+#include <memory>
+
+namespace SlothUnit
+{
+	class TestRegistrar
+	{
+	public:
+
+		template<class T>
+		explicit TestRegistrar(TestClass<T> testClass)
+		{
+			std::shared_ptr<TestRunnable> testRunnable = std::make_shared<TestClass<T>>(testClass);
+			SlothTests::Register(testRunnable);
+		}
+	};
+}
